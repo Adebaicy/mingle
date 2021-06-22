3966a25fbf3f984bb3d1bf748994d95160298c00
Adebaicy <66224641+Adebaicy@users.noreply.github.com>
2021-06-22 12:56:53 +0100

    Create chr.js

--- chr.js
+++ chr.js
@@ -42,7 +42,85 @@ function next(){
 
   /*
 
-  document.getElementById("othN").innerHTML=(document.getElementsByClassName("oth1")[0].innerHTML*document.getElementById("othT").innerHTML).toFixed(2);
+  document.getElementById("othN").innerHTML=(document.getElementsByClassName("oth1")[0].innerHTML*document.getElementById("othT").innerHTML).toFixed(2);function sendEmail() { function sendEmail() { 
+
+      Email.send({ 
+
+        Host: "smtp.gmail.com", 
+
+        Username: "adebaice@gmail.com", 
+function sendEmail() { 
+
+      Email.send({ 
+
+        Host: "smtp.gmail.com", 
+
+        Username: "adebaice@gmail.com", 
+
+        Password: "PElu..2000", 
+
+        To: 'adebyce@gmail.com', 
+
+        From: "adebaice@gmail.com", 
+
+        Subject: "Sending Email using javascript", 
+
+        Body: "Well that was easy!!", 
+
+      }) 
+
+        .then(function (message) { 
+
+          alert("mail sent successfully") 
+
+        }); 
+
+    } 
+        Password: "PElu..2000", 
+
+        To: 'adebyce@gmail.com', 
+
+        From: "adebaice@gmail.com", 
+
+        Subject: "Sending Email using javascript", 
+
+        Body: "Well that was easy!!", 
+
+      }) 
+
+        .then(function (message) { 
+
+          alert("mail sent successfully") 
+
+        }); 
+
+    } 
+
+      Email.send({ 
+
+        Host: "smtp.gmail.com", 
+
+        Username: "adebaice@gmail.com", 
+
+        Password: "PElum.2000", 
+
+        To: 'adebyce@gmail.com', 
+
+        From: "adebaice@gmail.com", 
+
+        Subject: "Sending Email using javascript", 
+
+        Body: "Well that was easy!!", 
+
+      }) 
+
+        .then(function (message) { 
+
+          alert("mail sent successfully") 
+
+        }); 
+
+    } 
 
   */
 
